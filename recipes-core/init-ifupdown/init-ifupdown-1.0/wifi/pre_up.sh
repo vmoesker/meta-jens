aeab5c66e8812901ebd81c5b01839a742a17616a
Jens Rehsack <sno@netbsd.org>
2014-09-04 13:17:13 +0200

    fix typo after functional correction

--- recipes-core/init-ifupdown/init-ifupdown-1.0/wifi/pre_up.sh
+++ recipes-core/init-ifupdown/init-ifupdown-1.0/wifi/pre_up.sh
@@ -4,7 +4,7 @@ test -r /etc/network/wifi/defaults && source /etc/network/wifi/defaults
 
 test -x /etc/network/if-pre-up.d/wpa-supplicant && /etc/network/if-pre-up.d/wpa-supplicant stop
 
-f [ -f /data/.shadow/.etc/wpa_supplicant.conf ] && grep -q ${wifi_module} ${wifi_modprobe_loaded_path}; then
+if [ -f /data/.shadow/.etc/wpa_supplicant.conf ] && grep -q ${wifi_module} ${wifi_modprobe_loaded_path}; then
     if [ $(lsmod | grep $wifi_module | wc -l) -eq 0 ]; then
 	    modprobe $wifi_module
     fi
