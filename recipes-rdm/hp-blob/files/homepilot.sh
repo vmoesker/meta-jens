4c8338ffd8216038778a883199aa31eeb9bdbddc
tobw <tobw@users.noreply.github.com>
2014-11-26 11:39:43 +0100

    use utf8 file encoding for homepilot

--- recipes-rdm/hp-blob/files/homepilot.sh
+++ recipes-rdm/hp-blob/files/homepilot.sh
@@ -3,4 +3,4 @@
 @HOMEPILOT_USER_HOME@/bin/init_appdir.sh
 
 cd @HOMEPILOT_BASE@/lib/java
-exec @JAVA_ELF@ -jar @HOMEPILOT_BASE@/lib/java/homepilot.jar
+exec @JAVA_ELF@ -Dfile.encoding=UTF-8 -jar @HOMEPILOT_BASE@/lib/java/homepilot.jar
