5370c36071166052088399fb781e8b43f63dc547
Jens Rehsack <sno@netbsd.org>
2014-12-18 17:15:44 +0100

    move shutdown of svc services to svc down script

--- recipes-extended/daemontools/daemontools-0.76/init-daemontools.sh
+++ recipes-extended/daemontools/daemontools-0.76/init-daemontools.sh
@@ -12,8 +12,22 @@ case "$1" in
 	start)
 		/etc/init.d/svscanboot &
 		;;
+	stop)
+		logger -s "Stopping services"
+		svc -d /etc/daemontools/service/*
+		sleep 2
+		svstat /etc/daemontools/service/* | logger -s
+		svc -dk /etc/daemontools/service/*
+		sleep 2
+		svstat /etc/daemontools/service/* | logger -s
+
+		killall supervise
+		killall readproctitle
+		killall svscan
+
+		;;
 	*)
-		echo "Usage $0 (start)" >&2
+		echo "Usage $0 (start|stop)" >&2
 		exit 1
 		;;
 esac
--- recipes-rdm/prd/prd-flash/flash-device.sh
+++ recipes-rdm/prd/prd-flash/flash-device.sh
@@ -150,14 +150,6 @@ then
 	logger "Force rebuild of volatiles.cache next boot"
         rm -f /etc/volatile.cache
 
-	logger "Stopping services"
-	svc -d /etc/daemontools/service/*
-	sleep 2
-	svstat /etc/daemontools/service/* | logger
-	svc -dk /etc/daemontools/service/*
-	sleep 2
-	svstat /etc/daemontools/service/* | logger
-
 	logger "Requesting reboot"
 	reboot
     elif [ $(echo ${ROOTDEV} | egrep 'p3$') ]
@@ -188,14 +180,6 @@ then
 	rm -f "${IMAGE_CONTAINER}"
 	logger "Force rebuild of volatiles.cache next boot"
         rm -f /etc/volatile.cache
-
-	logger "Stopping services"
-	svc -d /etc/daemontools/service/*
-	sleep 2
-	svstat /etc/daemontools/service/* | logger
-	svc -dk /etc/daemontools/service/*
-	sleep 2
-	svstat /etc/daemontools/service/* | logger
 
 	logger "Requesting reboot"
 	reboot
