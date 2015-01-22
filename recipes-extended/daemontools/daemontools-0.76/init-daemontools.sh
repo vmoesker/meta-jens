6682b4cccd9189a93d31855a591f39085d407af9
Jens Rehsack <sno@netbsd.org>
2015-01-22 10:33:11 +0100

    fix typo

--- recipes-extended/daemontools/daemontools-0.76/init-daemontools.sh
+++ recipes-extended/daemontools/daemontools-0.76/init-daemontools.sh
@@ -11,7 +11,7 @@
 case "$1" in
 	start)
 		mkdir /var/volatile/daemontools
-		(cd /etc && tar cf - daemontools) | (cd /var/volatile/daemontools && tar xf -)
+		(cd /etc && tar cf - daemontools) | (cd /var/volatile && tar xf -)
 		mount --bind /var/volatile/daemontools /etc/daemontools
 		/etc/init.d/svscanboot &
 		;;
