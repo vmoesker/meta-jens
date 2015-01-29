1d0b9ddf6222be057aecbc8b44ca34fe18d9bd5e
tobw <tobw@users.noreply.github.com>
2015-01-29 17:14:48 +0100

    sleep 90 seconds before setting ledbootup into ready state

--- recipes-rdm/ledctrl/ledctrl/ledbootup.sh
+++ recipes-rdm/ledctrl/ledctrl/ledbootup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Frank measured 45 seconds between LED turned into ready state and every application is runnning
-# sleep 60 seconds just to be sure
+# Frank measured >45 seconds between LED turned into ready state and every application is runnning
+# sleep 90 seconds just to be sure
 
-sleep 60s
+sleep 90s
 
 echo none >/sys/class/leds/boot/trigger
 echo 0 >/sys/class/leds/boot/brightness
