ee5ae169de6bed23316bbf5c8be8afe94847b39b
Jens Rehsack <sno@netbsd.org>
2015-05-28 17:20:49 +0200

    add script which runs fsck(8) at boot

---
+++ recipes-core/boot-fsck/files/boot-fsck.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:             boot-fsck
+# Required-Start:       
+# Required-Stop:      
+# Default-Start:
+# Default-Stop:
+# Short-Description:	runs recommended fsck(8) at boot
+### END INIT INFO
+
+case "$1" in
+        start)
+                fsck -a -p -t ext2
+                fsck -a -p -t ext4
+                ;;
+        stop)
+                ;;
+        *)
+                echo "Usage $0 (start|stop)" >&2
+                exit 1
+                ;;
+esac
+
+: exit 0
