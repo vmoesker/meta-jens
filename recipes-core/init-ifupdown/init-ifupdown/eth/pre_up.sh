2cc18b6f621583c53e9913e869d1ab3e6c18d5bb
tobw <tobw@users.noreply.github.com>
2015-12-01 15:41:49 +0100

    ethtool: adjust path to use /run/media
    instead of /media

--- recipes-core/init-ifupdown/init-ifupdown/eth/pre_up.sh
+++ recipes-core/init-ifupdown/init-ifupdown/eth/pre_up.sh
@@ -3,6 +3,6 @@
 ifdown wlan0
 test -x /etc/network/if-pre-up.d/wpa-supplicant && MODE=stop IF_WPA_CONF=/etc/wpa_supplicant.conf IFACE=wlan0 IF_WPA_DRIVER=wext /etc/network/if-pre-up.d/wpa-supplicant
 
-test -x /usr/sbin/ethtool && test -f /etc/ethtool-config && ETHTOOL_APPLY=/media/*/dont-apply-ethernet.settings && test $ETHTOOL_APPLY = "/media/*/dont-apply-ethernet.settings" && . /etc/ethtool-config && /usr/sbin/ethtool ${ETHTOOL_ARGS}
+test -x /usr/sbin/ethtool && test -f /etc/ethtool-config && ETHTOOL_APPLY=/run/media/*/dont-apply-ethernet.settings && test $ETHTOOL_APPLY = "/run/media/*/dont-apply-ethernet.settings" && . /etc/ethtool-config && /usr/sbin/ethtool ${ETHTOOL_ARGS}
 
 exit 0
