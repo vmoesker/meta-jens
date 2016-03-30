81b0fcd616b9200e67c9ecbf9bbb8caa3678235c
Jens Rehsack <sno@netbsd.org>
2016-03-30 14:39:21 +0200

    init-ifupdown: don't use static IP addresses failed DAD check (received responses on ARP requests)
    Prove whether configured fixed IP address is already in use in network
    (RFC2131, 4.4.1), even if primary target was dynamic configuration.
    
    Signed-off-by: Jens Rehsack <sno@netbsd.org>

--- recipes-core/init-ifupdown/init-ifupdown/activate.pl
+++ recipes-core/init-ifupdown/init-ifupdown/activate.pl
@@ -25,7 +25,21 @@ else
     $intf =~ s/#.*//g;
     while ( $intf =~ m,(?:iface\s${devname}-static.*?address\s+(\d+\.\d+\.\d+\.\d+).*?)(?:^$|\z),gsmx ) { push @addr, $1 }
     $status = "DYN";
-    ( scalar @addr == 1 ) and NetAddr::IP->new(@addr) > NetAddr::IP->new("0.0.0.0") and $status = "STATIC";
+    system(qw,/usr/bin/disable-error failed-DAD,);
+    if ( ( scalar @addr == 1 ) and NetAddr::IP->new(@addr) > NetAddr::IP->new("0.0.0.0") )
+    {
+        # redir STDOUT
+        open( my $old_stdout, ">&", STDOUT );
+        close(STDOUT);
+        open( STDOUT, ">&", STDERR );
+
+        # DAD check
+        system( qw,/usr/sbin/arping -c 5 -D -I,, $devname, @addr ) == 0 and $status = "STATIC";
+        $status eq "STATIC" or system(qw,/usr/bin/enable-error failed-DAD,);
+
+        # restore STDOUT
+        open( STDOUT, ">&", $old_stdout );
+    }
 }
 
 $status or die "Unknown status";
