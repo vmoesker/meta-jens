0355baa72a088127c07cb3e3f7625cc0c36de36b
Tobias Theophile <tobias.theophile@rademacher.de>
2016-04-12 13:57:31 +0200

    init-ifupdown: allow switching between ip configurations
    Currently there is no way to re-enable the dynamic ip configuration once
    a static ip is configured.
    
    Check the mapping configuration to allow switching between dynamic and
    static ip configuration. Also don't map the static ip configuration by
    default.
    
    Signed-off-by: Tobias Theophile <tobias.theophile@rademacher.de>

--- recipes-core/init-ifupdown/init-ifupdown/activate.pl
+++ recipes-core/init-ifupdown/init-ifupdown/activate.pl
@@ -20,25 +20,28 @@ if ( $devname =~ m/^wlan/
 }
 else
 {
-    my @addr;
-    my $intf = read_file("/etc/network/interfaces");
-    $intf =~ s/#.*//g;
-    while ( $intf =~ m,(?:iface\s${devname}-static.*?address\s+(\d+\.\d+\.\d+\.\d+).*?)(?:^$|\z),gsmx ) { push @addr, $1 }
     $status = "DYN";
     system(qw,/usr/bin/disable-error failed-DAD,);
-    if ( ( scalar @addr == 1 ) and NetAddr::IP->new(@addr) > NetAddr::IP->new("0.0.0.0") )
-    {
-        # redir STDOUT
-        open( my $old_stdout, ">&", STDOUT );
-        close(STDOUT);
-        open( STDOUT, ">&", STDERR );
+    if ( defined $mapping{"STATIC"} ) {
+        my @addr;
+        my $intf = read_file("/etc/network/interfaces");
+        $intf =~ s/#.*//g;
+        while ( $intf =~ m,(?:iface\s${devname}-static.*?address\s+(\d+\.\d+\.\d+\.\d+).*?)(?:^$|\z),gsmx ) { push @addr, $1 }
 
-        # DAD check
-        system( qw,/usr/sbin/arping -c 5 -D -I,, $devname, @addr ) == 0 and $status = "STATIC";
-        $status eq "STATIC" or system(qw,/usr/bin/enable-error failed-DAD,);
+        if ( ( scalar @addr == 1 ) and NetAddr::IP->new(@addr) > NetAddr::IP->new("0.0.0.0") )
+        {
+            # redir STDOUT
+            open( my $old_stdout, ">&", STDOUT );
+            close(STDOUT);
+            open( STDOUT, ">&", STDERR );
 
-        # restore STDOUT
-        open( STDOUT, ">&", $old_stdout );
+            # DAD check
+            system( qw,/usr/sbin/arping -c 5 -D -I,, $devname, @addr ) == 0 and $status = "STATIC";
+            $status eq "STATIC" or system(qw,/usr/bin/enable-error failed-DAD,);
+
+            # restore STDOUT
+            open( STDOUT, ">&", $old_stdout );
+        }
     }
 }
 
