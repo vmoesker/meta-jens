26a86f83cc1c8637845118c7069e5d0d5c24cd75
Tobias Theophile <tobias.theophile@rademacher.de>
2016-06-06 14:14:33 +0200

    prd-flash: improve regex to merge wpa_supplicant.conf
    Allow right curly brackets in network blocks (fixes ssid's containing })
    and use the first network block to save the user configuration.
    
    Signed-off-by: Tobias Theophile <tobias.theophile@rademacher.de>

--- recipes-rdm/prd/prd-flash/flash-device.sh
+++ recipes-rdm/prd/prd-flash/flash-device.sh
@@ -220,7 +220,7 @@ then
 		mkdir -p /run/media/${ROOT_DEV_NAME}${ROOT_DEV_SEP}3
 		mount -o bind / /run/media/${ROOT_DEV_NAME}${ROOT_DEV_SEP}3
 		cp /run/media/${ROOT_DEV_NAME}${ROOT_DEV_SEP}3/etc/wpa_supplicant.conf /data/.shadow/.etc/wpa_supplicant.conf.new
-		perl -le 'open(my $fh, "<", "/data/.shadow/.etc/wpa_supplicant.conf"); my $cnt; { local $/; $cnt = <$fh> }; $cnt =~ m/.*^(network=\{[^}]+\})/ms; print $1' >> /data/.shadow/.etc/wpa_supplicant.conf.new
+		perl -le 'open(my $fh, "<", "/data/.shadow/.etc/wpa_supplicant.conf"); my $cnt; { local $/; $cnt = <$fh> }; $cnt =~ m/^(network=\{.+?^\})/ms; print $1' >> /data/.shadow/.etc/wpa_supplicant.conf.new
 		test -f /etc/wpa_supplicant.conf.new && mv /etc/wpa_supplicant.conf.new /etc/wpa_supplicant.conf
 		umount /run/media/${ROOT_DEV_NAME}${ROOT_DEV_SEP}3
 	fi
