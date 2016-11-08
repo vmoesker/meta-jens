383087519c87b181c2f373c70b9551e6a5a1ed09
Volker <volker.moesker@rademacher.de>
2016-11-08 13:48:49 +0100

    map Z-Wave sticks directly w/o using z_get_tty
    Signed-off-by: Volker <volker.moesker@rademacher.de>

--- recipes-rdm/udev-hp/files/update_tty_zwave.sh
+++ recipes-rdm/udev-hp/files/update_tty_zwave.sh
@@ -10,13 +10,25 @@ tty_zwave="/dev/ttyZWave"
 
 if [ "$ACTION" = "add" ] && [ -n "$DEVNAME" ]
 then
+
     ttyid=`basename ${DEVPATH}`
-    tty=`basename /sys${DEVPATH}/${ttyid}*/tty[AU]*`
-    tty=$(cd $zway_home && env LD_LIBRARY_PATH="${zway_home}/libs:/opt/v8/lib" ./z-get-tty /dev/$tty)
+
+    if [ "$ID_VENDOR_ID" = "0658" ] && [ "$ID_MODEL_ID" = "0200" ]  # this is the new Z-Wave Stick
+    then
+        tty=`basename /sys${DEVPATH}/${ttyid}*/tty/tty[A]*`
+    elif [ "$ID_VENDOR_ID" = "0403" ] && [ "$ID_MODEL_ID" = "6001" ]  # this is the old FTDI based Z-Wave Stick
+    then
+        tty=`basename /sys${DEVPATH}/${ttyid}*/tty[U]*`
+    else  # no stick fits
+        exit 1
+    fi
+
+    tty=/dev/$tty
 
     echo "test -n \"$tty\" -a ! -c $tty_zwave && ln -fs $tty $tty_zwave" >>/tmp/update_tty
     test -n "$tty" -a ! -c $tty_zwave && ln -fs $tty $tty_zwave
     led_zwave
+
 elif [ "$ACTION" = "remove" ] && [ -n "$DEVNAME" ]
 then
     if [ -e $tty_zwave ]
